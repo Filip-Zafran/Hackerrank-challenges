810f1c612e1e5163dbf9be3180ec03d6f2520427
FichoZee <filip.zafran@gmail.com>
2020-04-25 17:06:18 +0200

    add [200~lastIndexNumber~

--- How many socks/socks.js
+++ How many socks/socks.js
@@ -2,7 +2,7 @@ const ar = [1, 1, 2, 1, 2, 1, 3, 2, 3, 1];
 
 // 1. SORT ARRAY
 const sortedArray = ar.sort();
-// console.log(sortedArray);
+console.log(sortedArray);
 
 const sameNumbersArray = [];
 let numberOfSameNumbers = 0;
@@ -10,7 +10,10 @@ let numberOfSameNumbers = 0;
 // 2.  PUSH SAME NUMBERS IN AN ARRAY
 // this loop doesnt add the last number as it
 
-for (i = 0; i < sortedArray.length; i++) {
+// let lastIndexNumberIntoIndex = 0;
+let lastIndexNumber = 0;
+
+for (i = lastIndexNumber; i < sortedArray.length; i++) {
   if (sortedArray[i] == sortedArray[i + 1]) {
     const sameNumber = sortedArray[i];
     sameNumbersArray.push(sameNumber);
@@ -30,18 +33,22 @@ numberOfSameNumbers = sameNumbersArray.length / 2;
 
 // 3.B) STORE LAST INDEX
 
-// let lastIndexNumberArray = [];
-// lastIndexNumber.push(sameNumbersArray.length);
+let lastIndexNumberArray = [];
+lastIndexNumberArray.push(sameNumbersArray.length - 1);
 
-// console.log("lastIndexNumberArray", lastIndexNumberArray);
+console.log("lastIndexNumberArray", lastIndexNumberArray);
 
-// // SUM OF THE ARRAY + 1 for ZERO INDEX
-// var lastIndexNumber = lastIndexNumberArray.reduce(function (a, b) {
-//   return a + b;
-// }, 0);
+// SUM OF THE ARRAY - 1 for ZERO INDEX
+lastIndexNumber = lastIndexNumberArray.reduce(function (a, b) {
+  return a + b;
+}, 0);
+// last number before the bracket ')' is the initial value
+//  initial value should Last index
+console.log("lastIndexNumber", lastIndexNumber);
 
-// console.log("lastIndexNumber", lastIndexNumber);
+// lastIndexNumberIntoIndex = "[" + lastIndexNumber + "]";
 
+// console.log("lastIndexNumberIntoIndex", lastIndexNumberIntoIndex);
 console.log("sameNumbersArray", sameNumbersArray.length / 2);
 console.log("final number", numberOfSameNumbers);
 
@@ -52,15 +59,11 @@ let endArray = [];
 endArray.push(Math.floor(numberOfSameNumbers));
 console.log(endArray);
 
-// 5. START LOOP FROM LAST INDEX
+// 5. START LOOP FROM LAST INDEX OR PUT LAST INDEX IN SAME SCOPE (?)
 
 // 6. SUM UP THE LAST ARRAY
-
 var challengeResult = endArray.reduce(function (a, b) {
   return a + b;
 }, 0);
-
-// 6. EXPLANATION:
-// https: //www.tutorialrepublic.com/faq/how-to-find-the-sum-of-an-array-of-numbers-in-javascript.php
 
 console.log("Challenge result:", challengeResult);
